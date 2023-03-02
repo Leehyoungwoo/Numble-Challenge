9e5a2b2314c1ae3ad0009a654ba22b531e3855e4
Leehyoungwoo <dlguddn3024@gmail.com>
2023-03-02 16:08:15 +0900

    First commit

---
+++ Numble-Challenge/src/test/java/Numble/NumbleChallenge/NumbleChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package Numble.NumbleChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumbleChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
