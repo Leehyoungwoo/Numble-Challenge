af1d3470e3dd295adab71d1c13baf0ed406a3a9b
Leehyoungwoo <dlguddn3024@gmail.com>
2023-03-02 16:47:19 +0900

    Feature: 상품을 저장할 Item 객체 구현

---
+++ Numble-Challenge/src/main/java/Numble/NumbleChallenge/domain/item/Item.java
@@ -0,0 +1,20 @@
+package Numble.NumbleChallenge.domain.item;
+
+import lombok.Data;
+
+@Data
+public class Item {
+    private Long id;
+    private String ItemName;
+    private Integer price;
+    private Integer quantity;
+
+    public Item() {
+    }
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        ItemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
