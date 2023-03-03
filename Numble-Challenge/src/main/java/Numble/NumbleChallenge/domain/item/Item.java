a8a991c0d5159744560e1fe834bb30102c28629b
Leehyoungwoo <dlguddn3024@gmail.com>
2023-03-03 15:59:39 +0900

    Fix: Item 클래스 피드백 반영 - 불필요한 setter, getter 제거, @NoArgsConstructor 어노테이션으로 기본 생성자 대체, 변수명 수정,

--- Numble-Challenge/src/main/java/Numble/NumbleChallenge/domain/item/Item.java
+++ Numble-Challenge/src/main/java/Numble/NumbleChallenge/domain/item/Item.java
@@ -3,36 +3,26 @@ package Numble.NumbleChallenge.domain.item;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
+
 @AllArgsConstructor
 @Builder
+@NoArgsConstructor
 public class Item {
     private Long id;
-    private String ItemName;
+    private String name;
     private Integer price;
     private Integer quantity;
 
-    public Item() {
-    }
-
-    public Item(String itemName, Integer price, Integer quantity) {
-        ItemName = itemName;
-        this.price = price;
-        this.quantity = quantity;
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public Long getId() {
         return id;
     }
 
-    public String getItemName() {
-        return ItemName;
-    }
-
-    public Integer getPrice() {
-        return price;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
+    public Item update(Long id, String name, Integer price, Integer quantity) {
+        return new Item(id, name, price, quantity);
     }
 }
