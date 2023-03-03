6e0d9e2da6a57428f620f2fcedfcc98b03dc5db1
Leehyoungwoo <dlguddn3024@gmail.com>
2023-03-03 16:26:27 +0900

    Feature: MemberRepository 인터페이스 구현

---
+++ Numble-Challenge/src/main/java/Numble/NumbleChallenge/domain/member/MemberRepository.java
@@ -0,0 +1,14 @@
+package Numble.NumbleChallenge.domain.member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+
+    Member save(Member member);
+
+    Optional<Member> findById(Long id);
+
+    List<Member> findAll();
+
+}
