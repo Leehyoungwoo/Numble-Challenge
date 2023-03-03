578e1ae6b7f71cd214e2dbd3f81fb9131e9c6404
Leehyoungwoo <dlguddn3024@gmail.com>
2023-03-03 16:00:47 +0900

    Feature: 피드백 반영 - ItemRepository 인터페이스 구현으로 OCP 원칙 고수

---
+++ Numble-Challenge/src/main/java/Numble/NumbleChallenge/domain/item/ItemRepository.java
@@ -0,0 +1,12 @@
+package Numble.NumbleChallenge.domain.item;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ItemRepository {
+    Item save(Item item);
+
+    Optional<Item> findById(Long id);
+
+    List<Item> findAll();
+}
