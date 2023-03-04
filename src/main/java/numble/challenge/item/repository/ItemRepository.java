1effd0a5cd0585b9f664ecb7626636125be917b5
Hyoungwoo <dlguddn3024@gmail.com>
2023-03-05 01:59:29 +0900

    :poop: Fix: Jpa를 사용하기 위해 @Repository 어노테이션으로 ItemRepository 인터페이스 수정

---
+++ src/main/java/numble/challenge/item/repository/ItemRepository.java
@@ -0,0 +1,10 @@
+package numble.challenge.item.repository;
+
+import numble.challenge.domain.model.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+}
