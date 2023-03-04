cd1801cb3b1d9447ff3b270148e70e37030abe28
Hyoungwoo <dlguddn3024@gmail.com>
2023-03-05 01:53:47 +0900

    :sparkles: Feat: 상품요청의 정보를 저장하는 ItemRequestSaveDto 객체 구현

---
+++ src/main/java/numble/challenge/item/controller/dto/ItemRequestSaveDto.java
@@ -0,0 +1,26 @@
+package numble.challenge.item.controller.dto;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Min;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class ItemRequestSaveDto {
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private Integer price;
+
+    @NotNull
+    @Min(0)
+    private Integer quantity;
+}
