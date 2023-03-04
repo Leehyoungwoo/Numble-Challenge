eaf20d8fb8aac140d6a764d2ffe78f6477b1d872
Hyoungwoo <dlguddn3024@gmail.com>
2023-03-05 01:22:25 +0900

    :fire: Remove: NotEnoughStockException 재구현, 이전 클래스 제거

--- src/main/java/Numble/NumbleChallenge/domain/order/exception/NotEnoughStockException.java
+++
@@ -1,7 +0,0 @@
-package Numble.NumbleChallenge.domain.order.exception;
-
-public class NotEnoughStockException extends RuntimeException{
-    public NotEnoughStockException(String message) {
-        super(message);
-    }
-}
