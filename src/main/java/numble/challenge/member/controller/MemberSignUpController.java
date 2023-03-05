b725b08612cacc081cd4a8403e99be866700a292
Hyoungwoo <dlguddn3024@gmail.com>
2023-03-05 23:09:15 +0900

    :sparkles: Feat: MemberSignUpController 클래스에 요청을 받아 회원가입폼을 보여주는 메서드 signUpForm 클래스 구현
    - 회원가입 폼 sign-up-form.html 작성, PostMapping후 보여주는 html 파일명 log-in-form.html로 수정

--- src/main/java/numble/challenge/member/controller/MemberSignUpController.java
+++ src/main/java/numble/challenge/member/controller/MemberSignUpController.java
@@ -5,6 +5,8 @@ import numble.challenge.domain.model.entity.Member;
 import numble.challenge.member.controller.dto.MemberSaveDto;
 import numble.challenge.member.service.MemberService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -14,11 +16,16 @@ public class MemberSignUpController {
 
     private final MemberService memberService;
 
-    @PostMapping("/api/member")
-    public String create(@RequestBody MemberSaveDto memberSaveDto) {
-        memberService.join(Member.toEntity(memberSaveDto));
+    @GetMapping("/api/member/sign-up/")
+    public String signUpForm(Model model) {
+        model.addAttribute("member", new Member());
         return "sign-up-form";
     }
 
+    @PostMapping("/api/member")
+    public String signUp(@RequestBody MemberSaveDto memberSaveDto) {
+        memberService.join(Member.toEntity(memberSaveDto));
+        return "log-in-form";
+    }
 
 }
