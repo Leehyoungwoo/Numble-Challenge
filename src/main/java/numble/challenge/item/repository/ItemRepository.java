349c74e38001f7bb842e896eda0792aee5bf0748
Hyoungwoo <dlguddn3024@gmail.com>
2023-03-08 04:32:40 +0900

    :sparkles: Feat: 상품이름을 통한 검색을 위해 ItemRepository 인터페이스에 메서드 추가

--- src/main/java/numble/challenge/item/repository/ItemRepository.java
+++ src/main/java/numble/challenge/item/repository/ItemRepository.java
@@ -2,9 +2,14 @@ package numble.challenge.item.repository;
 
 import numble.challenge.domain.model.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findByNameContaining(String name);
 
 }
