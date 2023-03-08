5621a5708f79415806ccd436fc7e9ad75de9e2cb
Hyoungwoo <dlguddn3024@gmail.com>
2023-03-08 14:27:20 +0900

    :sparkles: Feat: 상품 컨트롤러에 저장 양식을 보여주는 saveForm 메서드 구현
    -상품 정보 수정 양식을 보여주는 updateForm 메서드 구현
    
    -상품 정보 수정 update 메서드 구현

--- src/main/java/numble/challenge/item/controller/ItemController.java
+++ src/main/java/numble/challenge/item/controller/ItemController.java
@@ -3,8 +3,13 @@ package numble.challenge.item.controller;
 import lombok.RequiredArgsConstructor;
 import numble.challenge.domain.model.entity.Item;
 import numble.challenge.item.controller.dto.ItemRequestSaveDto;
+import numble.challenge.item.controller.dto.ItemRequestUpdateDto;
+import numble.challenge.item.repository.ItemRepository;
 import numble.challenge.item.service.ItemService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -13,10 +18,30 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class ItemController {
 
     private final ItemService itemService;
+    private final ItemRepository itemRepository;
 
-    @PostMapping("/api/item")
+    @GetMapping("/api/item/save")
+    public String saveForm(Model model) {
+        model.addAttribute("item", new Item());
+        return "itemSave-form";
+    }
+
+    @PostMapping("/api/item/save")
     public String save(@RequestBody ItemRequestSaveDto itemRequestSaveDto) {
         itemService.save(Item.toEntity(itemRequestSaveDto));
-        return "viewName";
+        return "redirect:/";
+    }
+
+    @GetMapping("/api/item/{id}/update")
+    public String updateForm(@PathVariable Long id, Model model) {
+        Item item = itemRepository.findById(id).orElse(null);
+        model.addAttribute("item", item);
+        return "itemUpdate-form";
+    }
+
+    @PostMapping("/api/item/{id}/update")
+    public String update(@PathVariable Long id, ItemRequestUpdateDto itemRequestUpdateDto) {
+        itemService.update(id, itemRequestUpdateDto);
+        return "redirect:/";
     }
 }
