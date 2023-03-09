93e6096b52311c5f9d2ca1b46c8889a12052f06f
Hyoungwoo <dlguddn3024@gmail.com>
2023-03-09 17:52:57 +0900

    :poop: Fix: JpaRepository를 상속해 사용하지 않게 된 저장소 로직 주석 처리

--- src/main/java/numble/challenge/member/repository/MemoryMemberRepository.java
+++ src/main/java/numble/challenge/member/repository/MemoryMemberRepository.java
@@ -1,30 +1,30 @@
-package numble.challenge.member.repository;
-
-import numble.challenge.domain.model.entity.Member;
-import org.springframework.stereotype.Repository;
-
-import java.util.*;
-
-public class MemoryMemberRepository  {
-
-    private static final Map<Long, Member> memberStore = new HashMap<>();
-    private static Long sequence = 0L;
-
-    public Member save(Member member) {
-        member.setId(++sequence);
-        memberStore.put(sequence, member);
-        return member;
-    }
-
-    public Optional<Member> findById(Long id) {
-        return Optional.ofNullable(memberStore.get(id));
-    }
-
-    public List<Member> findAll() {
-        return new ArrayList<>(memberStore.values());
-    }
-
-    public void clearMemberStore() {
-        memberStore.clear();
-    }
-}
+//package numble.challenge.member.repository;
+//
+//import numble.challenge.domain.model.entity.Member;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.*;
+//
+//public class MemoryMemberRepository  {
+//
+//    private static final Map<Long, Member> memberStore = new HashMap<>();
+//    private static Long sequence = 0L;
+//
+//    public Member save(Member member) {
+//        member.setId(++sequence);
+//        memberStore.put(sequence, member);
+//        return member;
+//    }
+//
+//    public Optional<Member> findById(Long id) {
+//        return Optional.ofNullable(memberStore.get(id));
+//    }
+//
+//    public List<Member> findAll() {
+//        return new ArrayList<>(memberStore.values());
+//    }
+//
+//    public void clearMemberStore() {
+//        memberStore.clear();
+//    }
+//}
