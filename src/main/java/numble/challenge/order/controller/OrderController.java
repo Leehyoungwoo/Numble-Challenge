5e2a5f594df9b4e834a79e52318e677d87188585
Hyoungwoo <dlguddn3024@gmail.com>
2023-03-11 21:59:04 +0900

    :sparkles: Feat: 주문폼을 보여주는 createForm 메서드 구현
    - 주문을 생성하는 기능 create 구현
    
    - 주문을 취소하는 기능 cancel 메서드 구현

--- src/main/java/numble/challenge/order/controller/OrderController.java
+++ src/main/java/numble/challenge/order/controller/OrderController.java
@@ -1,5 +1,37 @@
 package numble.challenge.order.controller;
 
+import lombok.RequiredArgsConstructor;
+import numble.challenge.domain.model.entity.Order;
+import numble.challenge.order.service.OrderService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequiredArgsConstructor
 public class OrderController {
 
+    private final OrderService orderService;
+
+    @GetMapping("/api/order/new")
+    public String createForm(Model model) {
+        model.addAttribute("order", new Order());
+        return "orderForm";
+    }
+
+    @PostMapping("/api/order/new")
+    public String create(@RequestParam Long memberId, @RequestParam Long itemId, @RequestParam int count, Model model) {
+        orderService.createOrder(memberId,itemId,count);
+        model.addAttribute("message", "주문이 완료되었습니다.");
+        return "order-complete";
+    }
+
+    @PostMapping("/api/order/{orderId}/cancel")
+    public String cancel(@PathVariable Long orderId) {
+        orderService.cancelOrder(orderId);
+        return "redirect:/";
+    }
 }
